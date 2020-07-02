8b514b86f8610b534d1f839e2b13205195dea2a6
orjon <github@orjon.com>
2020-07-02 10:14:33 +0100

    new branch

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-//@router GET api/profile
-// 
-router.get('/', (req,res) => res.send('Profile route'));
+// import Profile and User models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+
+//@router GET api/profile/me
+// get current user profile
+// access: private
+router.get('/me',auth, async (req,res) => {
+  try {
+    // Find matching profile.
+    const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+
+    // if no profile return 400 error.
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user'})
+    }
+
+    // otherwise send profile
+    res.json(profile);
+
+  } catch(error){
+    console.error(error.message);
+    res.status(500).send('Server Error')
+  }
+});
+
+// res.send('Profile route'));
 
 module.exports = router;
