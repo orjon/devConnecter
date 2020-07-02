d33af2653a7c038339062ecbb39e683773e03b23
orjon <github@orjon.com>
2020-07-02 10:34:30 +0100

    Get current user profile route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-//@router GET api/profile
-// 
-router.get('/', (req,res) => res.send('Profile route'));
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//GET api/profile/me
+// Get current users profile
+// Private
+router.get(
+  '/me',
+  auth,
+  async (req,res) => {
+    try {
+      const profile = await Profile.findOne({user : req.user.id}).populate('user',['name','avatar']);
+
+      if (!profile){
+        return res.status(400).json({ msg : 'No profile for this user'})
+      }
+
+      res.json(profile)
+    }catch(error){
+      console.error(error);
+      res.ststus(500).send('Server Error')
+    }
+    
+  });
 
 module.exports = router;
