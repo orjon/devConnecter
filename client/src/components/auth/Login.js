2b5789da181ca5e3bc5900bf5221351975a4a0e1
orjon <github@orjon.com>
2020-07-09 16:17:33 +0100

    Register user working on client side.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+  const [ formData, setFormData ] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { email, password } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    console.log('SUCCESS')
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'><i className='fas fa-user'></i> Sign into your account</p>
+      <form
+        className='form'
+        action='create-profile.html'
+        onSubmit= {e => onSubmit(e)} >
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required //html5 client side validation
+            value= { email }
+            onChange= {e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value= { password }
+            onChange= {e => onChange(e)} 
+          />
+        </div>
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Register'
+        />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
   )
 }
 
 export default Login;
+
