1491af54b04f78e49d1360d40e98809450b1dd6d
orjon <github@orjon.com>
2020-07-23 17:56:08 +0100

    Added profile frontend

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,80 @@
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner'
+import { getProfileById } from '../../actions/profile'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
+
+const Profile = ({getProfileById, profile: { profile, loading}, auth, match}) => {
+  useEffect(() => {
+    getProfileById(match.params.id)
+  }, [getProfileById, match.params.id])
+  return (
+    <Fragment>
+      {profile ===null || loading ?
+      <Spinner />: 
+      <Fragment>
+        <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
+        {auth.isAuthenticated && !auth.loading && auth.user._id === profile.user._id && (<Link className='btn btn-dark' to='/edit-profile'>Edit Profile</Link>)}
+        <div className='profile-grid my-1'>
+          <ProfileTop profile={profile} />
+          <ProfileAbout profile={profile} />
+
+          {/* Experiences */}
+          <div className='profile-exp bg-white p-2'>
+            <h2 className='text-primay'>Experience</h2>
+            {profile.experience.length > 0 ? (
+              <Fragment>
+                {profile.experience.map(experience => (
+                  <ProfileExperience 
+                    key={experience._id}
+                    experience={experience}
+                  />
+                ))}
+              </Fragment>
+            ) : (<h4>No experience listed</h4>)}
+          </div>
+
+          {/* Education */}
+          <div className='profile-edu bg-white p-2'>
+            <h2 className='text-primay'>Education</h2>
+            {profile.education.length > 0 ? (
+              <Fragment>
+                {profile.education.map(education => (
+                  <ProfileEducation 
+                    key={education._id}
+                    education={education}
+                  />
+                ))}
+              </Fragment>
+            ) : (<h4>No education listed</h4>)}
+          </div>
+
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername}/>
+          )}
+
+
+        </div>
+      </Fragment>}
+    </Fragment>
+  )
+}
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { getProfileById } )(Profile)
